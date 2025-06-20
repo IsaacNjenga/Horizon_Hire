1b5522d504c17eb4512154d2d9f9864d36db4e57
Isaac Njenga <ayzzoh20@gmail.com>
2025-06-20 15:05:40 +0300

    Defined the routes

--- horizon_backend/controllers/userController.js
+++ horizon_backend/controllers/userController.js
@@ -1,7 +1,15 @@
 import UserModel from "../models/User.js";
 
 const updateUser = async (req, res) => {
+  const { id } = req.query;
+  if (!id) return res.status(404).json({ message: "No ID specified" });
   try {
+    await UserModel.findByIdAndUpdate(
+      { _id: id },
+      { $set: req.body },
+      { new: true }
+    );
+    return res.status(201).json({ success: true });
   } catch (error) {
     console.log("There was a server error:", error);
     res.status(500).json({ success: false, error: error.message });
@@ -9,7 +17,15 @@ const updateUser = async (req, res) => {
 };
 
 const updateProfilePicture = async (req, res) => {
+  const { id } = req.query;
+  if (!id) return res.status(404).json({ message: "No ID specified" });
   try {
+    await UserModel.findByIdAndUpdate(
+      { _id: id },
+      { $set: req.body },
+      { new: true }
+    );
+    return res.status(201).json({ success: true });
   } catch (error) {
     console.log("There was a server error:", error);
     res.status(500).json({ success: false, error: error.message });
@@ -17,7 +33,12 @@ const updateProfilePicture = async (req, res) => {
 };
 
 const fetchUser = async (req, res) => {
+  const { id } = req.query;
+  if (!id) return res.status(404).json({ message: "No ID specified" });
   try {
+    const user = await UserModel.findById({ _id: id });
+    const userDetails = { ...user._doc, password: undefined };
+    return res.status(200).json({ success: true, userDetails });
   } catch (error) {
     console.log("There was a server error:", error);
     res.status(500).json({ success: false, error: error.message });
@@ -25,7 +46,11 @@ const fetchUser = async (req, res) => {
 };
 
 const deleteUser = async (req, res) => {
+  const { id } = req.query;
+  if (!id) return res.status(404).json({ message: "No ID specified" });
   try {
+    await UserModel.findByIdAndDelete({ _id: id });
+    return res.status(200).json({ success: true });
   } catch (error) {
     console.log("There was a server error:", error);
     res.status(500).json({ success: false, error: error.message });
